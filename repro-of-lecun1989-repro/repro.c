ce7b611b647f577dfe5b827a74875a5b3c12a5a2
ramvenkat98 <ramvenkat98@gmail.com>
2023-10-07 17:15:04 -0700

    Approximate repro

--- repro-of-lecun1989-repro/repro.c
+++ repro-of-lecun1989-repro/repro.c
@@ -261,14 +261,207 @@ void simple_conv_net(Tensor *X_tr, Tensor *Y_tr, Tensor *X_te, Tensor *Y_te, uns
         loss /= X_te->sizes[0];
         printf("Loss is %f, correct classification rate is %f\n", loss, (float)(correctly_classified) / X_te->sizes[0]);
     }
-
-
-
     deallocate_conv_layer_storage(&c);
     deallocate_layer_storage(&l);
     deallocate_tanh_layer_storage(&t1);
     deallocate_tanh_layer_storage(&t2);
 }
+
+void repro_conv_net(Tensor *X_tr, Tensor *Y_tr, Tensor *X_te, Tensor *Y_te, unsigned int epochs, float learning_rate) {
+    srand(20);
+    unsigned int samples = 1; epochs = 1; //  X_tr->sizes[0];
+    unsigned int m = 1; // batch size
+    unsigned int n = IMAGE_SIZE;
+    unsigned int k = NUM_CLASSES;
+    unsigned int input_channels_c1 = 1;
+    unsigned int output_channels_c1 = 12;
+    unsigned int filter_size = 5;
+    unsigned int stride = 2;
+    unsigned int l_padding = 2;
+    unsigned int r_padding = 1;
+    float pad_with = -1.0;
+    unsigned int dilation = 1;
+    unsigned int c1_output_size = 8;
+    // C1: Convolution layer with input (1 x 16 x 16 x 1) and output (1 x 8 x 8 x 12)
+    ConvLayer c1;
+    allocate_conv_layer_storage(&c1, m, n, input_channels_c1, output_channels_c1, filter_size, stride, l_padding, r_padding, pad_with, dilation);
+    init_from_normal_distribution(0.0, 1.0/(double)(filter_size), c1.W->storage, filter_size * filter_size * input_channels_c1 * output_channels_c1);
+
+    // Reshape C1 output and output gradient from (1 x 8 x 8 x 12) to (1 x (8 * 8 * 12))
+    unsigned int reshaped_c1_output_n = c1_output_size * c1_output_size * output_channels_c1;
+    unsigned int reshaped_c1_output_sizes[2] = {m, reshaped_c1_output_n};
+    Tensor reshaped_c1_output, reshaped_c1_output_gradients;
+    reshape(c1.output, reshaped_c1_output_sizes, 2, &reshaped_c1_output);
+
+    // T1: Tanh activation with input (1 x (8 * 8 * 12)) and same sized output
+    TanhLayer t1;
+    allocate_tanh_layer_storage(&t1, m, reshaped_c1_output_n);
+    reshape(t1.dX, c1.output->sizes, 4, &reshaped_c1_output_gradients);
+
+    // Reshape T1 output and output gradient from (1 x (8 * 8 * 12)) to (1 x 8 x 8 x 12)
+    Tensor reshaped_t1_output, reshaped_t1_output_gradients;
+    reshape(t1.Z, c1.output->sizes, 4, &reshaped_t1_output);
+
+    // C2: Convolution layer with input (1 x 8 x 8 x 12) and output (1 x 4 x 4 x 8).
+    ConvLayer c2;
+    unsigned int output_channels_c2 = 8;
+    unsigned int c2_output_size = 4;
+    allocate_conv_layer_storage(&c2, m, c1_output_size, output_channels_c1, output_channels_c2, filter_size, stride, l_padding, r_padding, pad_with, dilation);
+    init_from_normal_distribution(0.0, 1.0/((double)(filter_size) * sqrt(output_channels_c1)), c2.W->storage, filter_size * filter_size * output_channels_c1 * output_channels_c2);
+    reshape(c2.dX, reshaped_c1_output_sizes, 2, &reshaped_t1_output_gradients);
+
+    // Reshape C2 output and output gradient from (1 x 4 x 4 x 8) to (1 x (4 * 4 * 8))
+    unsigned int reshaped_c2_output_n = c2_output_size * c2_output_size * output_channels_c2;
+    unsigned int reshaped_c2_output_sizes[2] = {m, reshaped_c2_output_n};
+    Tensor reshaped_c2_output, reshaped_c2_output_gradients;
+    reshape(c2.output, reshaped_c2_output_sizes, 2, &reshaped_c2_output);
+
+    // T2: Tanh activation with input (1 x (4 * 4 * 8)) and same sized output
+    TanhLayer t2;
+    allocate_tanh_layer_storage(&t2, m, reshaped_c2_output_n);
+    reshape(t2.dX, c2.output->sizes, 4, &reshaped_c2_output_gradients);
+
+    // L1: Fully Connected Linear layer with input (1 x (4 * 4 * 8)) and output (1 x 30)
+    FullyConnectedLinearLayer l1;
+    unsigned int l1_k = 30;
+    allocate_layer_storage(&l1, m, reshaped_c2_output_n, l1_k);
+    init_from_normal_distribution(0.0, 1.0/(double)(sqrt(m * reshaped_c2_output_n)), l1.W->storage, m * reshaped_c2_output_n * l1_k);
+
+    // T3: Tanh activation with input (1 x 30) and same sized output
+    TanhLayer t3;
+    allocate_tanh_layer_storage(&t3, m, l1_k);
+
+    // L2: Fully Connected Linear layer with input (1 x 30) and output (1 x 10)
+    FullyConnectedLinearLayer l2;
+    allocate_layer_storage(&l2, m, l1_k, k);
+    init_from_normal_distribution(0.0, 1.0/(double)(sqrt(m * l1_k)), l2.W->storage, m * l1_k * k);
+
+    // T4: Tanh activation with input (1 x 10) and same output
+    TanhLayer t4;
+    allocate_tanh_layer_storage(&t4, m, k);
+    Tensor *dOutput = create_zero(2, t4.Z->sizes);
+
+    for (unsigned int i = 0; i < epochs; i++) {
+        printf("Starting epoch %u...\n", i);
+        for (unsigned int j = 0; j < samples; j++) {
+            Tensor x_unreshaped, y, x;
+            Indexer x_indices[4] = {{true, j, j + 1}, {true, 0, input_channels_c1}, {true, 0, IMAGE_SIZE}, {true, 0, IMAGE_SIZE}};
+            Indexer y_indices[2] = {{true, j, j + 1}, {true, 0, NUM_CLASSES}};
+            unsigned int x_sizes[4] = {1, IMAGE_SIZE, IMAGE_SIZE, 1};
+            init_view(X_tr, x_indices, &x_unreshaped);
+            init_view(Y_tr, y_indices, &y);
+            reshape(&x_unreshaped, x_sizes, 4, &x);
+            // forward pass
+            compute_conv_outputs(&c1, &x);
+            compute_tanh_outputs(&t1, &reshaped_c1_output);
+            compute_conv_outputs(&c2, &reshaped_t1_output);
+            compute_tanh_outputs(&t2, &reshaped_c2_output);
+            compute_outputs(&l1, t2.Z);
+            compute_tanh_outputs(&t3, l1.Z);
+            compute_outputs(&l2, t3.Z);
+            compute_tanh_outputs(&t4, l2.Z);
+            // loss
+            subtract(&y, t4.Z, 1, dOutput);
+            for (int i = 0; i < NUM_CLASSES; i++) {
+                // Note: I think we technically shouldn't divide by NUM_CLASSES, but anyway
+                // it's just a scalar factor, and it's meant to keep it consistent with the
+                // way the loss is defined in the repro
+                dOutput->storage[i] *= (float)(-2.0 / NUM_CLASSES);
+            }
+            compute_tanh_gradients(&t4, dOutput, l2.Z);
+            compute_gradients(&l2, t4.dX, t3.Z);
+            compute_tanh_gradients(&t3, l2.dX, l1.Z);
+            compute_gradients(&l1, t3.dX, t2.Z);
+            compute_tanh_gradients(&t2, l1.dX, &reshaped_c2_output);
+            compute_conv_gradients(&c2, &reshaped_c2_output_gradients, &reshaped_t1_output);
+            compute_tanh_gradients(&t1, &reshaped_t1_output_gradients, &reshaped_c1_output);
+            compute_conv_gradients(&c1, &reshaped_c1_output_gradients, &x);
+            
+            if (j == 0 && i == 0) {
+                printf("W_c1 = ("); print_tensor(c1.W); printf(")\n");
+                printf("W_c2 = ("); print_tensor(c2.W); printf(")\n");
+                printf("W_l1 = ("); print_tensor(l1.W); printf(")\n");
+                printf("W_l2 = ("); print_tensor(l2.W); printf(")\n");
+                printf("b1 = ("); print_tensor(l1.b); printf(")\n");
+                printf("b2 = ("); print_tensor(l2.b); printf(")\n");
+            }
+            /*
+            if (j == 7290 && i == 1) {
+                printf("x = ("); print_tensor(&x); printf(")\n");
+                printf("output = ("); print_tensor(t2.Z); printf(")\n");
+                printf("dOutput = ("); print_tensor(dOutput); printf(")\n");
+                printf("dT2 = ("); print_tensor(t2.dX); printf(")\n");
+                printf("dT1 = ("); print_tensor(t1.dX); printf(")\n");
+                printf("dW_c = ("); print_tensor(c.dW); printf(")\n");
+                printf("dW_l = ("); print_tensor(l.dW); printf(")\n");
+                printf("dB = ("); print_tensor(l.dB); printf(")\n");
+            }
+            */
+            subtract(l2.W, l2.dW, learning_rate, l2.W);
+            subtract(l2.b, l2.dB, learning_rate, l2.b);
+            subtract(l1.W, l1.dW, learning_rate, l1.W);
+            subtract(l1.b, l1.dB, learning_rate, l1.b);
+            subtract(c2.W, c2.dW, learning_rate, c2.W);
+            subtract(c1.W, c1.dW, learning_rate, c1.W);
+            // for (int i = 0; i < 5 * 5 * 12; i++) c.dW->storage[i] = 0;
+            /*
+            if (j == 7290 && i == 1) {
+                printf("newW_c = ("); print_tensor(c.W); printf(")\n");
+                printf("newW_l = ("); print_tensor(l.W); printf(")\n");
+                printf("newb = ("); print_tensor(l.b); printf(")\n");
+            }
+            */
+        }
+        printf("Eval on test set after epoch %u\n", i);
+        float loss = 0;
+        unsigned int correctly_classified = 0;
+        for (unsigned int j = 0; j < X_te->sizes[0]; j++) {
+            Tensor x_unreshaped, y, x;
+            Indexer x_indices[4] = {{true, j, j + 1}, {true, 0, input_channels_c1}, {true, 0, IMAGE_SIZE}, {true, 0, IMAGE_SIZE}};
+            Indexer y_indices[2] = {{true, j, j + 1}, {true, 0, NUM_CLASSES}};
+            unsigned int x_sizes[4] = {1, IMAGE_SIZE, IMAGE_SIZE, 1};
+            init_view(X_te, x_indices, &x_unreshaped);
+            init_view(Y_te, y_indices, &y);
+            reshape(&x_unreshaped, x_sizes, 4, &x);
+            compute_conv_outputs(&c1, &x);
+            compute_tanh_outputs(&t1, &reshaped_c1_output);
+            compute_conv_outputs(&c2, &reshaped_t1_output);
+            compute_tanh_outputs(&t2, &reshaped_c2_output);
+            compute_outputs(&l1, t2.Z);
+            compute_tanh_outputs(&t3, l1.Z);
+            compute_outputs(&l2, t3.Z);
+            compute_tanh_outputs(&t4, l2.Z);
+            subtract(&y, t4.Z, 1, dOutput);
+            unsigned int max_index = 0;
+            unsigned int correct_index = 0;
+            float max_likelihood = -1.5;
+            for (unsigned int k = 0; k < NUM_CLASSES; k++) {
+                loss += dOutput->storage[k] * dOutput->storage[k];
+                if (Y_te->storage[j * NUM_CLASSES + k] > 0.9) {
+                    correct_index = k;
+                }
+                if (t4.Z->storage[k] > max_likelihood) {
+                    max_likelihood = t4.Z->storage[k];
+                    max_index = k;
+                }
+            }
+            if (max_index == correct_index) {
+                correctly_classified += 1;
+            }
+        }
+        loss /= X_te->sizes[0];
+        printf("Loss is %f, correct classification rate is %f\n", loss, (float)(correctly_classified) / X_te->sizes[0]);
+    }
+    deallocate_conv_layer_storage(&c1);
+    deallocate_conv_layer_storage(&c2);
+    deallocate_layer_storage(&l1);
+    deallocate_layer_storage(&l2);
+    deallocate_tanh_layer_storage(&t1);
+    deallocate_tanh_layer_storage(&t2);
+    deallocate_tanh_layer_storage(&t3);
+    deallocate_tanh_layer_storage(&t4);
+}
+
 
 int main() {
     Tensor *X_tr, *Y_tr, *X_te, *Y_te;
@@ -279,7 +472,8 @@ int main() {
     print_sizes(X_te);
     print_sizes(Y_te); */
     // simple_tanh_regression(X_tr, Y_tr, X_te, Y_te, EPOCHS, 0.03f); // 0.0005f);
-    simple_conv_net(X_tr, Y_tr, X_te, Y_te, EPOCHS, 0.03f); // 0.03f);
+    // simple_conv_net(X_tr, Y_tr, X_te, Y_te, EPOCHS, 0.03f); // 0.03f);
+    repro_conv_net(X_tr, Y_tr, X_te, Y_te, EPOCHS, 0.03f);
     free_tensor(X_tr, true);
     free_tensor(Y_tr, true);
     free_tensor(X_te, true);
